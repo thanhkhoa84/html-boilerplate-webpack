5700e97cb866a0801c6c6d3a64deaaac801146f7
Khoa Nguyen <thanhkhoa84@gmail.com>
2021-08-08 12:48:27 +0700

    adding webpackbar

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const WebpackBar = require('webpackbar');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
@@ -68,6 +69,9 @@ module.exports = {
     ],
   },
   plugins: [
+    new WebpackBar({
+      color: 'orange',
+    }),
     ...createHtml(),
     new MiniCssExtractPlugin({
       filename: 'assets/css/[name].css',
