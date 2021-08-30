f25e0125b3f0e62a3b68cc5890042b3920466bf3
Khoa Nguyen <thanhkhoa84@gmail.com>
2021-08-30 15:03:42 +0700

    adding postcss plugins, create demo module

---
+++ src/assets/js/modules/Incrementer.js
@@ -0,0 +1,25 @@
+export default class Incrementer {
+  constructor(el) {
+    this.el = el;
+    this.setVars();
+    this.bindEvents();
+  }
+
+  setVars() {
+    this.counter = this.el.querySelector('[data-target]');
+    this.button = this.el.querySelector('[data-trigger]');
+  }
+
+  bindEvents() {
+    this.button.addEventListener('click', this.add);
+  }
+
+  cleanUp() {
+    this.button.removeEventListener('click', this.add);
+  }
+
+  add = () => {
+    const content = this.counter.innerHTML;
+    this.counter.innerHTML = parseInt(content) + 1;
+  };
+}
