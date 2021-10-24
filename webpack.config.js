a2f84aee02f87a918f8014c164270814cc9093ca
Khoa Nguyen <thanhkhoa84@gmail.com>
2021-10-24 10:42:39 +0700

    update webpcak

--- webpack.config.js
+++ webpack.config.js
@@ -2,95 +2,104 @@ const path = require('path');
 const fs = require('fs');
 const glob = require('glob');
 const WebpackBar = require('webpackbar');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BeautifyHtmlWebpackPlugin = require('beautify-html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-const PurgeCssPlugin = require('purgecss-webpack-plugin');
-const { ESBuildMinifyPlugin } = require('esbuild-loader')
+const globImporter = require("node-sass-glob-importer");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BeautifyHtmlWebpackPlugin = require("beautify-html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const PurgeCssPlugin = require("purgecss-webpack-plugin");
+const { ESBuildMinifyPlugin } = require("esbuild-loader");
 
-const isDevMode = process.env.NODE_ENV === 'development';
-const mode = isDevMode ? 'development' : 'production';
+const isDevMode = process.env.NODE_ENV === "development";
+const mode = isDevMode ? "development" : "production";
 
 // MODULE LOADER
 const pug = {
-  test: /\.pug$/,
-  exclude: [
-    path.resolve(__dirname, 'src/views/layout'),
-    path.resolve(__dirname, 'src/views/partials'),
-    path.resolve(__dirname, 'src/views/mixins'),
-  ],
-  use: [
-    'html-loader',
-    {
-      loader: 'pug-html-loader',
-      options: {
-        pretty: true,
-      },
-    },
-  ],
+	test: /\.pug$/,
+	exclude: [
+		path.resolve(__dirname, "src/views/layout"),
+		path.resolve(__dirname, "src/views/partials"),
+		path.resolve(__dirname, "src/views/mixins"),
+	],
+	use: [
+		"html-loader",
+		{
+			loader: "pug-html-loader",
+			options: {
+				pretty: true,
+			},
+		},
+	],
 };
+
 const js = {
-  test: /\.js$/,
-  loader: 'esbuild-loader',
+	test: /\.js$/,
+	loader: "esbuild-loader",
 };
+
 const scss = {
-  test: /\.(scss|sass|css)$/,
-  use: [
-    isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-    {
-      loader: 'css-loader',
-      options: {
-        url: true,
-        modules: true,
-      },
-    },
-    { loader: 'postcss-loader' },
-    {
-      loader: 'resolve-url-loader',
-      options: {
-        sourceMap: true,
-      },
-    },
-    {
-      loader: 'sass-loader',
-      options: {
-        sourceMap: true,
-      },
-    },
-    { loader: 'import-glob-loader' },
-  ],
+	test: /\.(scss|sass|css)$/,
+	use: [
+		isDevMode ? "style-loader" : MiniCssExtractPlugin.loader,
+		{
+			loader: "css-loader",
+			options: {
+				url: true,
+				// modules: true,
+			},
+		},
+		{ loader: "postcss-loader" },
+		{
+			loader: "resolve-url-loader",
+			options: {
+				sourceMap: true,
+			},
+		},
+		{
+			loader: "sass-loader",
+			options: {
+				sourceMap: true,
+				sassOptions: {
+					importer: globImporter(),
+				},
+			},
+		},
+	],
 };
+
 const image = {
-  test: /\.(jpeg|jpg|png|gif|svg|bmp)$/i,
-  type: 'asset/resource',
-  generator: {
-    filename: './assets/images/[name][ext]',
-  },
+	test: /\.(jpeg|jpg|png|gif|svg|bmp)$/i,
+	type: "asset/resource",
+	generator: {
+		filename: "./assets/images/[name][ext]",
+	},
 };
+
 const font = {
-  test: /\.(woff|woff2|eot|ttf|otf)$/i,
-  type: 'asset/resource',
-  generator: {
-    filename: './assets/fonts/[name][ext]',
-  },
+	test: /\.(woff|woff2|eot|ttf|otf)$/i,
+	type: "asset/resource",
+	generator: {
+		filename: "./assets/fonts/[name][ext]",
+	},
 };
+
 const rules = [pug, js, scss, image, font];
 
 // PLUGINS
 const createHtml = function () {
-  const pugs = fs
-    .readdirSync(path.resolve(__dirname, 'src/views'))
-    .filter((f) => /\.pug$/g.test(f));
-  return pugs.map((pug) => {
-    return new HtmlWebpackPlugin({
-      template: `src/views/${pug}`,
-      filename: pug.replace('pug', 'html'),
-      inject: true,
-      minify: false,
-    });
-  });
+	const pugs = fs
+		.readdirSync(path.resolve(__dirname, "src/views"))
+		.filter((f) => /\.pug$/g.test(f));
+	return pugs.map((pug) => {
+		return new HtmlWebpackPlugin({
+			template: `src/views/${pug}`,
+			filename: pug.replace("pug", "html"),
+			inject: true,
+			minify: false,
+		});
+	});
 };
+
 let plugins = [
   new WebpackBar({
     color: 'orange',
