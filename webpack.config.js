48d3b6a204e0c0d0d41e52cce8e553b3edb37837
Khoa Nguyen <thanhkhoa84@gmail.com>
2022-12-28 13:24:44 +0700

    update svg optimization

--- webpack.config.js
+++ webpack.config.js
@@ -192,7 +192,19 @@ const optimization = isDevMode
 									{
 										plugins: [
 											{
-												removeViewBox: false,
+												name: "preset-default",
+												params: {
+													overrides: {
+														removeViewBox: false,
+														addAttributesToSVGElement: {
+															params: {
+																attributes: [
+																	{ xmlns: "http://www.w3.org/2000/svg" },
+																],
+															},
+														},
+													},
+												},
 											},
 										],
 									},
